6ee0cc26f17da8bdf59d1cb274e1e7feee4fcbe3
zeanjos <pegananjos@gmail.com>
2025-06-12 11:09:26 -0300

    Adicionado banco de dados postgresql

--- app/settings.py
+++ app/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
+PASSKEY = os.environ['PASSKEY']
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -77,9 +80,13 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'carros',
+        'USER': 'postgres',
+        'PASSWORD': PASSKEY,
+        'HOST': 'localhost',
+        'PORT': '5432',
+        }
 }
 
 
