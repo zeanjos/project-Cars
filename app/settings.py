33cd6c2c7b8ed71b8b0dd46afb4f7d2e01ad054e
zeanjos <pegananjos@gmail.com>
2025-06-17 10:35:10 -0300

    Configuração de allowed_host

--- app/settings.py
+++ app/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-1w6a(+n#_@h!f-uuw#5(coy4!g1aqy5o=he53rkq*24r_o=eax
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
