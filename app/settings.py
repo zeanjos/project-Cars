cc149fc054df563afb35ea39c0ba4d0a1e89875d
zeanjos <pegananjos@gmail.com>
2025-06-19 11:06:04 -0300

    Meu Deus como eu odeio aws

--- app/settings.py
+++ app/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join( BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
